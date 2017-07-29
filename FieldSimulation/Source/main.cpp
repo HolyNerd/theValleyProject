45a9ba9134bc47504f57cdb69d2f397f3e637d0e
HolyNerd <ppavel.zab@gmail.com>
2017-07-30 00:55:29 +0300

    Draw canvas

--- FieldSimulation/Source/main.cpp
+++ FieldSimulation/Source/main.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
 
+#include <SDL2/SDL.h>
+#include <GL/glew.h>
+
 int main() {
 
-	std::cout << "FieldSImulationBranch" << std::endl;
-	std::cout << "Simulation:" << std::endl;
+	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
 
+	SDL_Window* window = SDL_CreateWindow("OpenGL Program", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_OPENGL);
+	SDL_GLContext context = SDL_GL_CreateContext(window);
+
+	GLenum status = glewInit();
+	bool isClosed = false;
+
+	if(status != GLEW_OK) {
+		std::cerr << "GLew failed to initialize!" << std::endl;
+	}
+
+	while(!isClosed) {
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		SDL_GL_SwapWindow(window);
+		SDL_Event e;
+
+		while(SDL_PollEvent(&e)) {
+			if(e.type == SDL_QUIT) {
+				isClosed = true;
+			}
+		}
+	}
+
+
+	SDL_GL_DeleteContext(context);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	return 0;
 }
